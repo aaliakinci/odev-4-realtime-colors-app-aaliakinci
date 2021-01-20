cb46d5d5ad48d9e34c1104df22d520e98a35b3e3
aaliakinci <aliakiinci@gmail.com>
2021-01-20 21:25:00 +0300

    userController improvoment

--- backend/Models/User.js
+++ backend/Models/User.js
@@ -4,13 +4,18 @@ const Schema = mongoose.Schema;
 const UserSchema = new Schema({
 	username: {
 		type: String,
-		required: true,
-		unique: true,
+		required: [true,'Kullanıcı adı boş bırakılamaz'],
+		unique:[ true,'Bu kullanıcı adı kullanılıyor'],
 	},
 	password: {
 		type: String,
-		required: true,
+		required: [true,'Şifre alanı boş bırakılamaz'],
 	},
+});
+UserSchema.post('save', function (error, _, next) {
+	next( error.code === 11000 
+		?   new Error('Bu kullanıcı adı zaten var')
+		:   error)
 });
 
 module.exports = mongoose.model('user', UserSchema);
--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -1,64 +1,70 @@
-const createError = require('http-errors')
+const createError = require('http-errors');
 const User = require('../Models/User');
 const bcrypt = require('bcryptjs');
-
-const createUser = async(req,res) => {
-	res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3001');
-
-	try {
-		const {username,password} = req.body;
-		bcrypt.hash('password', 8, async(err, hash) => {
-		const user = new User ({
-			username,
-			password:hash
-		});
-		const result = await user.save()
-		res.status(200).json(result);
-	});
-	} catch (error) {
-		res.json(error);
-	}
-}
-
-const loginUser = async(req,res) => {
-	res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3001');
-
+const jwt = require('jsonwebtoken');
+const createUser = async (req, res) => {
 	try {
 		const { username, password } = req.body;
+		bcrypt.hash(password, 8, (err, hash) => {
+			const user = new User({
+				username,
+				password: hash,
+			});
+			const promise = user.save();
+			promise
+				.then((result) => {
+					res.status(200).json(result);
+				})
+				.catch((err) => {
+					res.json({status:414,message:err.message});
+				});
+		});
+	} catch (error) {
+		res.json({ status: 414, message: error.message });
+	}
+};
+
+const loginUser = async (req, res) => {
+	try {
+		const { username, password } = req.body;
+
 		const user = await findUserForLogin(username, password);
 		const token = await createToken(user);
-		res.status(200).json({ user, token });
+
+		res.json({ user, token });
 	} catch (error) {
-		res.json(error);
+		res.json({ status: error.code, message: error.message });
 	}
-}
+};
 //find user
-const findUserForLogin = async(username,password) => {
-	const user = await User.findOne({ username });
-	if (!user) {
-		console.log('kullanıcı bulamadım');
-		throw createError(400, 'Girilen kullanıcı adı ya da parola hatalı');
+const findUserForLogin = async (username, password) => {
+	try {
+		const resultUser = await User.findOne({ username });
+
+		if (!resultUser) {
+			throw createError({ code: 404, message: 'Girilen kullanıcı hatalı' });
+		}
+		const checkPassword = await bcrypt.compare(password, resultUser.password);
+		console.log(checkPassword);
+		if (!checkPassword) {
+			throw createError({ code: 404, message: 'Girilen  parola hatalı' });
+		}
+		return resultUser;
+	} catch (error) {
+		throw error;
 	}
-	const checkPassword = bcrypt.compare(password, user.password);
-	if (!checkPassword) {
-		throw createError(400, 'Girilen email/şifre hatalı');
-	}
-	return user;
-}
+};
 //create token
 const createToken = async (user) => {
 	const token = await jwt.sign(
-		{ _id: user._id, username: user.username},
+		{ _id: user._id, username: user.username },
 		process.env.JWT_SECRET_KEY,
 		{ expiresIn: '2d' },
 	);
 	return token;
 };
 
-
-
-module.exports={
+module.exports = {
 	createUser,
-	loginUser
-}
-
+	loginUser,
+};
